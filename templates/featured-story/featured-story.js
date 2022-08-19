2798a84fa71633be5b1813e568ac805787176729
thi64146 <thi64146@adobe.com>
2022-08-19 13:59:48 -0600

    add missing section class to featured story template for init

--- templates/featured-story/featured-story.js
+++ templates/featured-story/featured-story.js
@@ -14,7 +14,7 @@ function init() {
   section.classList.add('section-1');
   nextSection.classList.add('section-2');
   const sectionWrapper = createTag('div', { class: 'featured-story-wrapper container' });
-  const featuredSection = createTag('div', {}, sectionWrapper);
+  const featuredSection = createTag('div', { class: 'section' }, sectionWrapper);
   section.insertAdjacentElement('afterend', featuredSection);
   const col1 = createTag('div', { class: 'col-1' }, section);
   const col2 = createTag('div', { class: 'col-2' }, nextSection);
