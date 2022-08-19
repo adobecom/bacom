8f315c26b4d52a04445d60a3e4ae67209f28577d
thi64146 <thi64146@adobe.com>
2022-08-19 15:44:10 -0600

    fixed featured story bg section inheritance to be static and not rely on section-metadata.

--- templates/featured-story/featured-story.js
+++ templates/featured-story/featured-story.js
@@ -14,13 +14,12 @@ function init() {
   section.classList.add('section-1');
   nextSection.classList.add('section-2');
   const sectionWrapper = createTag('div', { class: 'featured-story-wrapper container' });
-  const featuredSection = createTag('div', { class: 'section' }, sectionWrapper);
+  const featuredSection = createTag('div', { class: 'section', template: 'featured-story' }, sectionWrapper);
   section.insertAdjacentElement('afterend', featuredSection);
   const col1 = createTag('div', { class: 'col-1' }, section);
   const col2 = createTag('div', { class: 'col-2' }, nextSection);
   sectionWrapper.insertAdjacentElement('afterbegin', col1);
   sectionWrapper.insertAdjacentElement('beforeend', col2);
-  if (nextSection.style) featuredSection.style.backgroundColor = nextSection.style.backgroundColor;
 }
 
 init();
