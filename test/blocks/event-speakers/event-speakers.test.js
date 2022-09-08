e45b1b077a35a1b9e558dae43d7f7f5e8aeaa39a
Megan Thomas <methomas@adobe.com>
2022-09-07 17:06:11 -0700

    Add event speakers block MWPW-114644

---
+++ test/blocks/event-speakers/event-speakers.test.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-expressions */
+/* global describe it */
+
+import { expect } from '@esm-bundle/chai';
+import init from '../../../blocks/event-speakers/event-speakers.js';
+
+describe('Event Speakers', () => {
+  it('adds ellipsis and button for long description', () => {
+    document.body.innerHTML = `<div class="event-speakers"><div>
+      <div><picture></picture></div>
+      <div><h3>Firstname Lastname</h3></div>
+      <div><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa.</p></div>
+      <div>Open me</div>
+    </div></div>`;
+
+    init(document.querySelector('.event-speakers'));
+    expect(document.querySelector('.ellipsis')).to.exist;
+    document.querySelector('button').click();
+    expect(document.querySelector('.ellipsis')).to.be.null;
+  });
+
+  it('shows entire description if short', () => {
+    document.body.innerHTML = `<div class="event-speakers"><div>
+      <div><picture></picture></div>
+      <div>Firstname Lastname</div>
+      <div><p>Lorem ipsum dolor sit amet.</p></div>
+    </div></div>`;
+    const expected = `Firstname Lastname
+
+Lorem ipsum dolor sit amet.`;
+
+    init(document.querySelector('.event-speakers'));
+    expect(document.querySelector('.text').innerText).equals(expected);
+  });
+});
