55dc76f60542e43af2c96aad5cff465e15c389f5
Megan Thomas <methomas@adobe.com>
2022-11-07 14:59:54 -0800

    MWPW-118659 Add JP Font (#31)
    * MWPW-118659 Add JP Font
    
    * MWPW-118659 Set Japanese Font
    
    * Move to variables

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -28,6 +28,7 @@ const CONFIG = {
     '': { ietf: 'en-US', tk: 'hah7vzn.css' },
     de: { ietf: 'de-DE', tk: 'hah7vzn.css' },
     kr: { ietf: 'ko-KR', tk: 'zfo3ouc' },
+    jp: { ietf: 'ja-JP', tk: 'dvg6awq' },
   },
 };
 
