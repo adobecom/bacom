98b81bbac4ba84caeb81c665a6aab94b1fe2b116
Megan Thomas <methomas@adobe.com>
2022-11-23 10:31:21 -0800

    MWPW-115379 - Tree View

---
+++ blocks/tree-view/tree-view.js
@@ -0,0 +1,193 @@
+import { getLibs } from '../../scripts/utils.js';
+
+const BACOM_HOSTS = ['localhost', '--bacom--adobecom.hlx.page', '--bacom--adobecom.hlx.live', 'business.adobe.com'];
+
+export const isCurrentPage = (link) => {
+  const currentPath = window.location.pathname.replace('.html', '');
+
+  try {
+    const url = new URL(link);
+    const isBacomHost = url.host === '' || BACOM_HOSTS.some((bacomHost) => url.host.includes(bacomHost));
+
+    if (isBacomHost && url.pathname.replace('.html', '') === currentPath) return true;
+  } catch (e) {
+    console.log('Tree View error:', e);
+  }
+
+  return false;
+};
+
+const toggleSection = (menuitem) => {
+  const subList = menuitem.querySelector('ul');
+  const isHidden = subList.hidden;
+
+  menuitem.setAttribute('aria-expanded', isHidden);
+  subList.setAttribute('aria-expanded', isHidden);
+  subList.hidden = !isHidden;
+};
+
+const buttonKeydown = (event) => {
+  const { key, target } = event;
+  const ul = target.closest('ul');
+  const li = target.parentElement;
+
+  if (key === 'ArrowDown' || key === 'ArrowRight') {
+    event.preventDefault();
+
+    const firstButton = ul.querySelector('button');
+    const next = li.nextElementSibling?.querySelector('button') || firstButton;
+
+    next.focus();
+  }
+
+  if (key === 'ArrowUp') {
+    event.preventDefault();
+
+    const lastItem = ul.querySelector(':scope > li:last-of-type');
+    const prevItem = li.previousElementSibling || lastItem;
+
+    if (prevItem.getAttribute('aria-expanded') === 'true') {
+      prevItem.querySelector('li:last-child a').focus();
+    } else {
+      prevItem.querySelector('button').focus();
+    }
+  }
+
+  if (key === 'ArrowLeft') {
+    const lastItem = ul.querySelector(':scope > li:last-of-type');
+    const prevItem = li.previousElementSibling || lastItem;
+
+    prevItem.querySelector('button').focus();
+  }
+
+  if (key === 'Escape' && li.getAttribute('aria-expanded') === 'true') {
+    toggleSection(li);
+  }
+};
+
+const linkKeydown = (event) => {
+  const { key, target } = event;
+  const liTop = target.closest('li[role=menuitem]');
+  const liSub = target.parentElement;
+
+  if (key === 'ArrowUp') {
+    event.preventDefault();
+
+    const prevItem = liSub.previousElementSibling?.querySelector('a') || liTop.querySelector('button');
+
+    prevItem.focus();
+  }
+
+  if (key === 'ArrowDown') {
+    event.preventDefault();
+
+    const menuitem = liTop.nextElementSibling || target.closest('.top-list');
+    const nextItem = liSub.nextElementSibling?.querySelector('a') || menuitem.querySelector('button');
+
+    nextItem.focus();
+  }
+
+  if (key === 'ArrowRight') {
+    const menuitem = liTop.nextElementSibling || target.closest('.top-list');
+
+    menuitem.querySelector('button').focus();
+  }
+
+  if (key === 'ArrowLeft') {
+    const lastItem = target.closest('.top-list').querySelector(':scope > li:last-of-type');
+    const menuitem = liTop.previousElementSibling || lastItem;
+
+    menuitem.querySelector('button').focus();
+  }
+
+  if (key === 'Escape' && liTop.getAttribute('aria-expanded') === 'true') {
+    toggleSection(liTop);
+  }
+};
+
+const init = async (el) => {
+  const topList = el.querySelector('ul');
+
+  if (!topList) return;
+
+  const { createTag } = await import(`${getLibs()}/utils/utils.js`);
+  const subLists = topList.querySelectorAll('ul');
+  const isAccordion = subLists.length > 0;
+  const links = el.querySelectorAll('a');
+  const currentLinks = [...links].filter((link) => isCurrentPage(link));
+  const pageTop = document.querySelector('header')?.offsetHeight ?? 0;
+  const title = el.querySelector('h1, h2, h3, h4, h5, h6, p');
+  let titleId = title?.id;
+
+  if (!titleId) {
+    const id = title.textContent.trim().replaceAll(' ', '-');
+    title.id = id;
+    titleId = id;
+  }
+
+  const nav = createTag('nav', { 'aria-labelledby': titleId }, ...el.children);
+
+  el.append(nav);
+  el.classList.add(`${isAccordion ? 'accordion' : 'simple'}`);
+  topList.classList.add('top-list', 'body-S');
+  title?.classList.add('title', 'heading-XS');
+
+  if (currentLinks.length === 1) { currentLinks[0].classList.add('current-page'); }
+
+  links.forEach((link) => {
+    const anchor = link.href.split('#')?.[1];
+    const target = anchor ? document.querySelector(`#${anchor}`) : null;
+
+    if (target) {
+      link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetPosition = target.getBoundingClientRect().top ?? 0;
+        const offsetPosition = targetPosition + window.pageYOffset - pageTop;
+
+        window.scrollTo(0, offsetPosition);
+        target.setAttribute('tabindex', -1);
+        target.focus();
+      });
+    }
+
+    if (isAccordion) {
+      link.role = 'menuitem';
+      link.addEventListener('keydown', linkKeydown);
+    }
+  });
+
+  if (isAccordion) {
+    topList.role = 'menu';
+
+    const topListItems = topList.querySelectorAll(':scope > li');
+
+    topListItems.forEach((topListItem) => {
+      topListItem.role = 'menuitem';
+      topListItem.setAttribute('aria-haspopup', 'menu');
+      topListItem.setAttribute('aria-expanded', false);
+
+      const label = topListItem.textContent.split('\n')[0];
+      const id = label.trim().replaceAll(' ', '-');
+      const button = createTag('button', { id }, label);
+      const subList = topListItem.querySelector('ul');
+
+      topListItem.innerHTML = '';
+      topListItem.append(button, subList);
+      subList.role = 'menu';
+      subList.setAttribute('aria-labelledby', id);
+      subList.setAttribute('aria-expanded', false);
+      subList.hidden = true;
+
+      button.addEventListener('click', (event) => toggleSection(event.target.closest('li[role=menuitem')));
+      button.addEventListener('keydown', buttonKeydown);
+    });
+
+    const subListItems = topList.querySelectorAll('li ul li');
+
+    subListItems.forEach((subListItem) => {
+      subListItem.role = 'presentation';
+    });
+  }
+};
+
+export default init;
---
+++ test/blocks/tree-view/tree-view.test.js
@@ -0,0 +1,204 @@
+/* eslint-disable no-unused-expressions */
+/* global describe it beforeEach before */
+
+import { readFile } from '@web/test-runner-commands';
+import { expect } from '@esm-bundle/chai';
+import sinon from 'sinon';
+import { setLibs } from '../../../scripts/utils.js';
+
+const { default: init, isCurrentPage } = await import('../../../blocks/tree-view/tree-view.js');
+
+setLibs('libs');
+
+describe('Tree View', () => {
+  describe('simple', () => {
+    beforeEach(async () => {
+      document.body.innerHTML = await readFile({ path: './mocks/simple.html' });
+    });
+
+    it('adds styles', async () => {
+      await init(document.querySelector('.tree-view'));
+      expect(document.querySelector('.tree-view.simple')).to.exist;
+    });
+
+    it('has current page', async () => {
+      await init(document.querySelector('.tree-view'));
+      expect(document.querySelector('.tree-view a.current-page')).to.exist;
+    });
+
+    it('does not show current page styles with anchors', async () => {
+      const block = document.querySelector('.tree-view.anchors');
+      await init(block);
+      expect(block.querySelector('a.current-page')).to.not.exist;
+    });
+
+    it('changes focus on click of anchor link', async () => {
+      const block = document.querySelector('.tree-view.anchors');
+      await init(block);
+      block.querySelector('a').click();
+      expect(document.activeElement).to.equal(document.querySelector('#heading'));
+    });
+  });
+
+  it('isCurrentPage catches error', () => {
+    sinon.spy(console, 'log');
+    isCurrentPage('/relative-link');
+    expect(console.log.args[0][0]).to.equal('Tree View error:');
+    console.log.restore();
+  });
+
+  describe('accordion', () => {
+    const down = new KeyboardEvent('keydown', { key: 'ArrowDown' });
+    const up = new KeyboardEvent('keydown', { key: 'ArrowUp' });
+    const right = new KeyboardEvent('keydown', { key: 'ArrowRight' });
+    const left = new KeyboardEvent('keydown', { key: 'ArrowLeft' });
+    const esc = new KeyboardEvent('keydown', { key: 'Escape' });
+    let li;
+    let menuitem1;
+    let menuitem2;
+    let menuitem3;
+    let link1;
+    let link2;
+    let lastLink;
+
+    before(async () => {
+      document.body.innerHTML = await readFile({ path: './mocks/accordion.html' });
+      await init(document.querySelector('.tree-view'));
+
+      li = document.querySelector('ul > li');
+      menuitem1 = document.querySelector('#menuitem1 button');
+      menuitem2 = document.querySelector('#menuitem2 button');
+      menuitem3 = document.querySelector('#menuitem3 button');
+      link1 = document.querySelector('#link1');
+      link2 = document.querySelector('#link2');
+      lastLink = document.querySelector('#last-link');
+    });
+
+    it('adds styles', () => {
+      expect(document.querySelector('.tree-view.accordion')).to.exist;
+    });
+
+    it('starts closed', () => {
+      expect(li.querySelector('ul').getAttribute('hidden')).to.exist;
+      expect(li.getAttribute('aria-expanded')).to.equal('false');
+    });
+
+    it('toggles section on click', () => {
+      menuitem1.click();
+      expect(li.querySelector('ul').getAttribute('hidden')).to.not.exist;
+      expect(li.getAttribute('aria-expanded')).to.equal('true');
+
+      menuitem1.click();
+      expect(li.querySelector('ul').getAttribute('hidden')).to.exist;
+      expect(li.getAttribute('aria-expanded')).to.equal('false');
+    });
+
+    describe('keyboard navigation', () => {
+      it('down arrow moves focus to next menuitem when on menu', () => {
+        document.querySelector('button').dispatchEvent(down);
+        expect(document.activeElement).to.equal(document.querySelector('ul > li:nth-child(2) button'));
+      });
+
+      it('down arrow wraps when closed', () => {
+        document.querySelector('ul > li:last-child button').dispatchEvent(down);
+        expect(document.activeElement).to.equal(document.querySelector('button'));
+      });
+
+      it('down arrow moves focus to next link when open', () => {
+        menuitem1.click();
+        link1.dispatchEvent(down);
+        expect(document.activeElement).to.equal(link2);
+        menuitem1.click();
+      });
+
+      it('down arrow wraps when open', () => {
+        menuitem3.click();
+        lastLink.dispatchEvent(down);
+        expect(document.activeElement).to.equal(menuitem1);
+        menuitem3.click();
+      });
+
+      it('up arrow moves focus to previous menu item when closed', () => {
+        document.querySelector('ul > li:last-child button').dispatchEvent(up);
+        expect(document.activeElement).to.equal(document.querySelector('ul > li:nth-last-child(2) button'));
+      });
+
+      it('up arrow wraps when closed', () => {
+        document.querySelector('button').dispatchEvent(up);
+        expect(document.activeElement).to.equal(document.querySelector('ul > li:last-child button'));
+      });
+
+      it('up arrow moves focus to previous link when open', () => {
+        menuitem1.click();
+        link2.dispatchEvent(up);
+        expect(document.activeElement).to.equal(link1);
+        menuitem1.click();
+      });
+
+      it('up arrow moves focus from first link to menuitem when open', () => {
+        menuitem1.click();
+        link1.dispatchEvent(up);
+        expect(document.activeElement).to.equal(menuitem1);
+        menuitem1.click();
+      });
+
+      it('up arrow moves focus from menuitem to last link when open', () => {
+        menuitem1.click();
+        menuitem2.dispatchEvent(up);
+        expect(document.activeElement).to.equal(document.querySelector('#link4'));
+        menuitem1.click();
+      });
+
+      it('right arrow moves focus to next menuitem from menu', () => {
+        menuitem1.dispatchEvent(right);
+        expect(document.activeElement).to.equal(menuitem2);
+      });
+
+      it('right arrow moves focus to next menuitem from link', () => {
+        link1.dispatchEvent(right);
+        expect(document.activeElement).to.equal(menuitem2);
+      });
+
+      it('right arrow wraps', () => {
+        lastLink.dispatchEvent(right);
+        expect(document.activeElement).to.equal(menuitem1);
+      });
+
+      it('left arrow moves focus to previous menuitem from menu', () => {
+        menuitem2.dispatchEvent(left);
+        expect(document.activeElement).to.equal(menuitem1);
+      });
+
+      it('left arrow moves focus to previous menuitem from link', () => {
+        document.querySelector('#link5').dispatchEvent(left);
+        expect(document.activeElement).to.equal(menuitem1);
+      });
+
+      it('left arrow wraps from menu', () => {
+        menuitem1.dispatchEvent(left);
+        expect(document.activeElement).to.equal(menuitem3);
+      });
+
+      it('left arrow wraps from link', () => {
+        link1.dispatchEvent(left);
+        expect(document.activeElement).to.equal(menuitem3);
+      });
+
+      it('closes on escape on menuitem', () => {
+        menuitem1.click();
+        menuitem1.dispatchEvent(esc);
+        expect(document.querySelector('#menuitem1').getAttribute('aria-expanded')).to.equal('false');
+        menuitem1.dispatchEvent(esc);
+        expect(document.querySelector('#menuitem1').getAttribute('aria-expanded')).to.equal('false');
+      });
+
+      it('closes on escape on link', () => {
+        menuitem1.click();
+        link1.dispatchEvent(esc);
+        expect(document.querySelector('#menuitem1').getAttribute('aria-expanded')).to.equal('false');
+        link1.dispatchEvent(esc);
+        expect(document.querySelector('#menuitem1').getAttribute('aria-expanded')).to.equal('false');
+      });
+    });
+  });
+});
