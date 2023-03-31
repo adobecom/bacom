7deb66e6942e37d14ed21aa2c6a28a43916fa117
Megan Thomas <methomas@adobe.com>
2023-03-31 10:18:25 -0700

    Revert "Enable cta config (#91)" (#92)
    This reverts commit 03f7a61c96722d9162f34c0887a06cff8318d080.

--- blocks/chat-cta/chat-cta.js
+++ blocks/chat-cta/chat-cta.js
@@ -1,27 +1,5 @@
-export default async function init(el) {
-  const block = el;
-  const contentUrl = el.dataset?.content;
-
-  if (contentUrl) {
-    const resp = await fetch(contentUrl);
-    if (!resp.ok) return;
-    const text = await resp.text();
-    if (text) {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(text, 'text/html');
-      const ctaBody = doc.querySelector('.chat-cta > div');
-      if (!ctaBody) return;
-      block.append(ctaBody);
-      const link = ctaBody.querySelector('a');
-      const { pathname, hash } = new URL(link.href, window.location.origin);
-      link.dataset.modalPath = pathname;
-      link.dataset.modalHash = hash;
-      link.href = hash;
-      link.className = 'modal link-block';
-    }
-  }
-
-  const cta = block.children[0].children[0];
+const init = async (el) => {
+  const cta = el.children[0].children[0];
   cta.classList.add('cta-chat-sticky');
 
   const a = cta.querySelector('a');
@@ -34,4 +12,6 @@ export default async function init(el) {
   window.addEventListener('milo:modal:closed', () => {
     cta.classList.remove('cta-hidden');
   });
-}
+};
+
+export default init;
