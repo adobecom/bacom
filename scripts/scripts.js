14eb03c5737ad5a8ce44c7f4d7afeff1f0535bcf
Jason Slavin <slavin@adobe.com>
2023-03-31 10:29:39 -0700

    Removing undefined error referencing the metaCta when not present

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -156,11 +156,13 @@ const miloLibs = setLibs(LIBS);
 (async function loadPage() {
   const { loadArea, loadDelayed, loadLana, setConfig, createTag } = await import(`${miloLibs}/utils/utils.js`);
   const metaCta = document.querySelector('meta[name="chat-cta"]');
-  const isMetaCtaDisabled = metaCta.content === 'off';
-  if (metaCta && !isMetaCtaDisabled && !document.querySelector('.chat-cta')) {
-    const chatDiv = createTag('div', { class: 'chat-cta meta-cta', 'data-content': metaCta.content });
-    const lastSection = document.body.querySelector('main > div:last-of-type');
-    if (lastSection) lastSection.insertAdjacentElement('beforeend', chatDiv);
+  if (metaCta && !document.querySelector('.chat-cta')) {
+    const isMetaCtaDisabled = metaCta?.content === 'off';
+    if (!isMetaCtaDisabled) {
+      const chatDiv = createTag('div', { class: 'chat-cta meta-cta', 'data-content': metaCta.content });
+      const lastSection = document.body.querySelector('main > div:last-of-type');
+      if (lastSection) lastSection.insertAdjacentElement('beforeend', chatDiv);
+    }
   }
   setConfig({ ...CONFIG, miloLibs });
   loadLana({ clientId: 'bacom' });
