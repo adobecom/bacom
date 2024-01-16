5c91730616488d236e5da621e835f80cdfb43784
Jason Slavin <slavin@adobe.com>
2024-01-16 11:58:15 -0800

    Adding functionality, styles, still need to add additional tests and error handling

--- blocks/redirects-formatter/redirects-formatter.js
+++ blocks/redirects-formatter/redirects-formatter.js
@@ -71,22 +71,50 @@ export default async function init(el) {
   if (!resp.ok) return;
   const { data } = await resp.json();
 
+  const redirectsContainer = createTag('section', { class: 'redirects-container' });
+
+  // Header
+  const header = createTag('h1');
+  header.innerText = 'Redirect Formatting Tool';
+
+  // Checkboxes
+  const checkBoxesHeader = createTag('p', { class: 'cb-label' });
+  checkBoxesHeader.innerText = 'Select Locales';
   const checkBoxes = await createLocaleCheckboxes(data);
   const checkBoxesContainer = createTag('div', { class: 'checkbox-container' }, checkBoxes);
   const selectAllCB = createTag('button', { class: 'select-all-cb' });
   selectAllCB.innerText = SELECT_ALL_REGIONS;
-  checkBoxesContainer.append(selectAllCB);
+  const checkBoxesArea = createTag('section', { class: 'cb-area' }, [checkBoxesHeader, selectAllCB, checkBoxesContainer]);
 
-  const redirectsContainer = createTag('section', { class: 'redirects-container' });
+  // Text input area
+  const inputAreaContainer = createTag('section', { class: 'input-container' });
   const textAreaInput = createTag('textarea', { class: 'redirects-text-area', id: 'redirects-input', name: 'redirects-input' });
   const taiLabel = createTag('label', { class: 'io-label', for: 'redirects-input' });
   taiLabel.innerText = 'Place urls here';
+  const submitButton = createTag('button', { class: 'process-redirects' });
+  submitButton.innerText = 'Process Redirects';
+  inputAreaContainer.append(taiLabel, submitButton, textAreaInput);
+
+  // Text output Area
+  const outputAreaContainer = createTag('section', { class: 'output-container' });
   const textAreaOutput = createTag('textarea', { class: 'redirects-text-area', id: 'redirects-output', name: 'redirects-output' });
+  textAreaOutput.setAttribute('readonly', 'true');
   const taoLabel = createTag('label', { class: 'io-label', for: 'redirects-output' });
   taoLabel.innerText = 'Localized results appear here';
-  const ioContainer = createTag('div', { class: 'io-container' });
-  const submitButton = createTag('button', { class: 'process=redirects' });
-  submitButton.innerText = 'Process Redirects';
+  const copyButton = createTag('button', { class: 'copy' });
+  copyButton.innerText = 'Copy to clipboard';
+  outputAreaContainer.append(taoLabel, copyButton, textAreaOutput);
+
+  // Event listeners
+  selectAllCB.addEventListener('click', () => {
+    const allNotSelected = selectAllCB.innerText === SELECT_ALL_REGIONS;
+
+    document.querySelectorAll('.locale-checkbox').forEach((cb) => {
+      cb.checked = allNotSelected;
+    });
+
+    selectAllCB.innerText = allNotSelected ? DESELECT_ALL_REGIONS : SELECT_ALL_REGIONS;
+  });
 
   submitButton.addEventListener('click', () => {
     const locales = [...document.querySelectorAll("[type='checkbox']")].reduce((rdx, cb) => {
@@ -103,18 +131,25 @@ export default async function init(el) {
     textAreaOutput.value = outputString;
   });
 
-  selectAllCB.addEventListener('click', () => {
-    const allNotSelected = selectAllCB.innerText === SELECT_ALL_REGIONS;
-
-    document.querySelectorAll('.locale-checkbox').forEach((cb) => {
-      cb.checked = allNotSelected;
-    });
-
-    selectAllCB.innerText = allNotSelected ? DESELECT_ALL_REGIONS : SELECT_ALL_REGIONS;
+  copyButton.addEventListener('click', () => {
+    if (!navigator?.clipboard) return;
+    const redirects = textAreaOutput.value;
+    navigator.clipboard.writeText(redirects).then(
+      () => {
+        copyButton.innerText = 'Copied';
+        setTimeout(() => {
+          copyButton.innerText = 'Copy to clipbaord';
+        }, 1500);
+      },
+      () => {
+        copyButton.innerText = 'Error!';
+        setTimeout(() => {
+          copyButton.innerText = 'Copy to clipbaord';
+        }, 1500);
+      },
+    );
   });
 
-  ioContainer.append(taiLabel, textAreaInput, taoLabel, textAreaOutput);
-  redirectsContainer.append(ioContainer, submitButton);
-
-  el.append(checkBoxesContainer, redirectsContainer);
+  redirectsContainer.append(checkBoxesArea, inputAreaContainer, outputAreaContainer);
+  el.append(header, redirectsContainer);
 }
