2d7f680e791b17577d73b7b599aafa5e2780beb0
Megan Thomas <methomas@adobe.com>
2024-01-19 15:52:57 -0800

    add request

--- scripts/aa-university.js
+++ scripts/aa-university.js
@@ -1,21 +1,21 @@
 export default function submitAAUniversity() {
   console.log('submitAAUniversity');
-  // const firstName = document.querySelector('input[name="FirstName"]');
-  // const lastName = document.querySelector('input[name="LastName"]');
-  // const email = document.querySelector('input[name="Email"]');
-  // const country = document.querySelector('input[name="Country"]');
+  const firstName = document.querySelector('input[name="FirstName"]');
+  const lastName = document.querySelector('input[name="LastName"]');
+  const email = document.querySelector('input[name="Email"]');
+  const country = document.querySelector('input[name="Country"]');
 
-  // fetch('https://us-central1-adobe---aa-university.cloudfunctions.net/register', {
-  //   method: 'POST',
-  //   body: JSON.stringify({
-  //     first_name: firstName.value,
-  //     last_name: lastName.value,
-  //     email: email.value,
-  //     university: 'none',
-  //     country: country.value,
-  //   }),
-  // })
-  //   .catch((error) => {
-  //     window.lana.log('Marketo AA University Error:', error);
-  //   });
+  fetch('https://us-central1-adobe---aa-university.cloudfunctions.net/register', {
+    method: 'POST',
+    body: JSON.stringify({
+      first_name: firstName.value,
+      last_name: lastName.value,
+      email: email.value,
+      university: 'none',
+      country: country.value,
+    }),
+  })
+    .catch((error) => {
+      window.lana.log('Marketo AA University Error:', error);
+    });
 }
