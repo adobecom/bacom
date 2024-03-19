9dafb5f9b6b0c635d0a54e88efa0769ea30806a0
Megan Thomas <methomas@adobe.com>
2024-03-19 11:16:36 -0700

    add styles to head and cleanup

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,6 @@
-/*
- * Copyright 2022 Adobe. All rights reserved.
- * This file is licensed to you under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License. You may obtain a copy
- * of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- * OF ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- */
-
 import { setLibs } from './utils.js';
 
 const LIBS = '/libs';
-const STYLES = ['/styles/styles.css'];
 const CONFIG = {
   imsClientId: 'bacom',
   local: {
@@ -168,21 +155,9 @@ const loadStyle = (path) => {
   eagerLoad(marquee.querySelector('img'));
 }());
 
-/*
- * ------------------------------------------------------------
- * Edit below at your own risk
- * ------------------------------------------------------------
- */
-
 const miloLibs = setLibs(LIBS);
 
-(function loadStyles() {
-  const paths = [`${miloLibs}/styles/styles.css`];
-  if (STYLES) {
-    paths.push(...(Array.isArray(STYLES) ? STYLES : [STYLES]));
-  }
-  paths.forEach(loadStyle);
-}());
+loadStyle(`${miloLibs}/styles/styles.css`);
 
 (async function loadPage() {
   const { loadArea, loadLana, setConfig, createTag } = await import(`${miloLibs}/utils/utils.js`);
