d4b407cfb43e47c2ce0bcacb3c3967dea869059e
Robert Bogos <robert.adobe.github@gmail.com>
2024-06-11 15:18:26 +0300

    eslint fix

--- scripts/utils.js
+++ scripts/utils.js
@@ -4,7 +4,7 @@ export const [setLibs, getLibs] = (() => {
     (prodLibs, location) => {
       libs = (() => {
         const { hostname, search } = location || window.location;
-        if (!['.hlx.', '.stage.', 'local'].some(i => hostname.includes(i))) return prodLibs;
+        if (!['.hlx.', '.stage.', 'local'].some((i) => hostname.includes(i))) return prodLibs;
         const branch = new URLSearchParams(search).get('milolibs') || 'main';
         if (branch === 'local') return 'http://localhost:6456/libs';
         return branch.includes('--') ? `https://${branch}.hlx.live/libs` : `https://${branch}--milo--adobecom.hlx.live/libs`;
