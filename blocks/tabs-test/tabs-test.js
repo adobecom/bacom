7805180011bd39f38fb3ca0e92465301b288357d
Jason Slavin <slavin@adobe.com>
2024-06-17 13:54:42 -0700

    Prep for JS only handoff

--- blocks/tabs-test/tabs-test.js
+++ blocks/tabs-test/tabs-test.js
@@ -1,4 +1,35 @@
 export default async function init() {
+  const tabOverrideStyles = `
+    @media screen and (max-width: 600px) {
+      .tablist-features-section .tabs div[role="tablist"] .tab-list-container {
+        flex-direction: column;
+      }
+
+      .tablist-features-section .tabs div[role="tablist"] button[aria-selected="true"] {
+        background: none;
+        background-color: #d5d5d5;
+      }
+
+      .tablist-features-section .tabs.center div[role="tablist"] .tab-list-container {
+        width: 100%;
+        margin: 30px;
+      }
+
+      .tablist-features-section .tabs div[role="tablist"] button {
+        border-radius: 0;
+        font-size: 24px;
+      }
+
+      .tablist-features-section .tabs div[role="tablist"] {
+        margin: 0;
+        box-shadow: 0 -1px 0 inset var(--tabs-border-color);
+      }
+    }
+  `;
+  const tabsOverideStylesheet = document.createElement('style');
+  tabsOverideStylesheet.textContent = tabOverrideStyles;
+  document.head.append(tabsOverideStylesheet);
+
   const tabs = document.querySelectorAll('.tabList button');
   tabs.forEach((tab) => {
     tab.addEventListener('click', () => {
