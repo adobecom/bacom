4b4e70c2da41700191c1f3f50961ac96941c02fc
Jason Slavin <slavin@adobe.com>
2024-09-05 09:13:28 -0700

    Adding the ability to have a group param added to the sandbox POST request. There is a default, so we do not want to send anything if we do not find the metadata

--- scripts/aa-university.js
+++ scripts/aa-university.js
@@ -3,7 +3,7 @@ export default function registerAAUniversity() {
   const lastName = document.querySelector('input[name="LastName"]');
   const email = document.querySelector('input[name="Email"]');
   const country = document.querySelector('select[name="Country"]');
-  const group = document.querySelector('meta[name="sandboxGroup"]');
+  const group = document.querySelector('meta[name="sandboxgroup"]');
 
   const postBody = {
     first_name: firstName?.value,
@@ -20,4 +20,6 @@ export default function registerAAUniversity() {
     body: JSON.stringify(postBody),
   })
     .catch((error) => window.lana.log(`Marketo AA University Error: ${error}`));
+
+  return postBody;
 }
