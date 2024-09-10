5a76b49170c0c22fc623f849ed2b4b2636dfac61
Jason Slavin <slavin.jhs@gmail.com>
2024-09-10 11:55:17 -0700

    Revert "MWPW-154363: Add param anaylitics sandbox"

--- scripts/aa-university.js
+++ scripts/aa-university.js
@@ -3,23 +3,16 @@ export default function registerAAUniversity() {
   const lastName = document.querySelector('input[name="LastName"]');
   const email = document.querySelector('input[name="Email"]');
   const country = document.querySelector('select[name="Country"]');
-  const group = document.querySelector('meta[name="sandboxgroup"]');
-
-  const postBody = {
-    first_name: firstName?.value,
-    last_name: lastName?.value,
-    email: email?.value,
-    university: 'none',
-    country: country?.value,
-  };
-
-  if (group) postBody.group = group.content;
 
   fetch('https://us-central1-adobe---aa-university.cloudfunctions.net/register', {
     method: 'POST',
-    body: JSON.stringify(postBody),
+    body: JSON.stringify({
+      first_name: firstName?.value,
+      last_name: lastName?.value,
+      email: email?.value,
+      university: 'none',
+      country: country?.value,
+    }),
   })
     .catch((error) => window.lana.log(`Marketo AA University Error: ${error}`));
-
-  return postBody;
 }
