30a83e4961c8fd32abb37f55210e13130f70b715
Megan Thomas <methomas@adobe.com>
2024-09-19 10:52:10 -0700

    Remove hlx.page and hlx.live stage mappings

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -126,11 +126,7 @@ const CONFIG = {
   ],
   useDotHtml: true,
   dynamicNavKey: 'bacom',
-  stageDomainsMap: {
-    'business.stage.adobe.com': { 'business.adobe.com': 'origin' },
-    '--bacom--adobecom.hlx.live': { 'business.adobe.com': 'origin' },
-    '--bacom--adobecom.hlx.page': { 'business.adobe.com': 'origin' },
-  },
+  stageDomainsMap: { 'business.stage.adobe.com': { 'business.adobe.com': 'origin' } },
 };
 
 const eagerLoad = (img) => {
