9e9ebaaa4aba04dd3418b02e97af55cc5301fe71
Okan Sahin <osahin@adobe.com>
2024-11-18 10:21:50 +0100

    Consume stage again

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -158,7 +158,7 @@ const loadStyle = (path) => {
 export function setLibs(location) {
   const { hostname, search } = location;
   if (!['.aem.', '.hlx.', '.stage.', 'local'].some((i) => hostname.includes(i))) return '/libs';
-  const branch = new URLSearchParams(search).get('milolibs') || 'hlx5-upgrade';
+  const branch = new URLSearchParams(search).get('milolibs') || 'main';
   if (branch === 'local') return 'http://localhost:6456/libs';
   return branch.includes('--') ? `https://${branch}.aem.live/libs` : `https://${branch}--milo--adobecom.aem.live/libs`;
 }
