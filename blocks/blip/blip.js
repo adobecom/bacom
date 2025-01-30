4cb129ae3d0681e20b0e13e50c1ea7d04d71766e
Jason Slavin <slavin@adobe.com>
2025-01-30 14:31:04 -0800

    Added some logic for summary cards. Not in the right place, need to run after click, but works for our purposes

--- blocks/blip/blip.js
+++ blocks/blip/blip.js
@@ -30,6 +30,19 @@ const headers = {
   Connection: 'keep-alive',
 };
 
+const stubSummarySources = [
+  {
+    title: 'Lorem 1',
+    summary: 'Lorem ipsum dolert alum signes mesun tabila',
+    link: 'https://business.adobe.com/resources/holiday-shopping-report.html',
+  },
+  {
+    title: 'Lorem 2',
+    summary: 'Lorem ipsum dolert alum signes mesun tabila',
+    link: 'https://business.adobe.com/customer-success-stories/ey-case-study.html',
+  },
+];
+
 const requestFromLLM = async (userInput) => {
   const input = `[INST]${userInput}[/INST]`;
 
@@ -45,14 +58,30 @@ const requestFromLLM = async (userInput) => {
   return response.text();
 };
 
+const createSources = async (sources, createTag) => {
+  const cards = sources.map(async (source) => {
+    const title = createTag('h4', {}, source.title);
+    const summary = createTag('p', {}, source.summary);
+    const link = createTag('a', { href: source.link }, source.link);
+
+    const cardPage = await fetch(source.link);
+    const html = await cardPage.text();
+    const doc = new DOMParser().parseFromString(html, 'text/html');
+    const meta = doc.querySelector('meta[property="og:image"]');
+    const image = createTag('img', { src: meta.content });
+
+    return createTag('div', { class: 'blip-card' }, [image, title, summary, link]);
+  });
+
+  return Promise.all(cards);
+};
+
 export default async function init(el) {
   const { createTag } = await import(`${LIBS}/utils/utils.js`);
 
   const [inputContainer, gated] = el.querySelectorAll(':scope > div');
 
-  const isGated = gated.children[1].textContent === 'true';
-  console.log(isGated);
-
+  // const isGated = gated.children[1].textContent === 'true';
   gated.remove();
 
   const placeHolderText = inputContainer.children[0].textContent;
@@ -61,12 +90,18 @@ export default async function init(el) {
   const button = createTag('button', { class: 'blip-submit' }, 'submit');
   const input = createTag('input', { type: 'text', class: 'blip-input', name: 'blip-input', id: 'blip-input', placeholder: placeHolderText });
   inputContainer.classList.add('blip-input-container');
-  const summaryContainer = createTag('section', { class: 'blip-summary' });
+
+  const summaryContainer = createTag('section', { class: 'blip-summary hidden' });
   inputContainer.append(input, button);
-  el.append(summaryContainer);
+
+  const sourceCards = await createSources(stubSummarySources, createTag);
+  const summarySourcesContainer = createTag('section', { class: 'blip-sources hidden' }, sourceCards);
+  el.append(summaryContainer, summarySourcesContainer);
 
   button.addEventListener('click', async () => {
     console.log('click');
+    el.querySelectorAll('.hidden').forEach((item) => item.classList.remove('hidden'));
+
     const userInput = input.value;
     summaryContainer.classList.add('loading');
     summaryContainer.innerText = 'Loading';
